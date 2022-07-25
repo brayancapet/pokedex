a09ce90ace97ce299d6065408639b780d84161f8
Brayan Capet <bcapet@bcapet.jechange.ivalley>
2022-07-25 16:53:42 +0200

    api connectée

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,91 +9,32 @@ import { PokeapiService } from 'src/app/pokeapi.service';
 })
 export class HomePageComponent implements OnInit {
 
-  cards: Card[] = [
-    {
-      name: 'charizard',
-      id: 6,
-      front_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-      back_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png',
-      shiny_pic_front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png',
-      shiny_pic_back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/6.png',
-      types: ['fire', 'flying'],
-      weight: 905,
-      height: 17,
-      attack: 84,
-      default: true,
-    },
-    {
-      name: 'pikachu',
-      id: 25,
-      front_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
-      back_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png',
-      shiny_pic_front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png',
-      shiny_pic_back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/25.png',
-      types: ['electric'],
-      weight: 60,
-      height: 4,
-      attack: 55,
-      default: true,
-    },
-    {
-      name: 'arceus',
-      id: 493,
-      front_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png',
-      back_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/493.png',
-      shiny_pic_front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/493.png',
-      shiny_pic_back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/493.png',
-      types: ['normal'],
-      weight: 3200,
-      height: 32,
-      attack: 120,
-      default: true,
-    },
-    {
-      name: 'ratata',
-      id: 19,
-      front_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png',
-      back_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/19.png',
-      shiny_pic_front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/19.png',
-      shiny_pic_back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/19.png',
-      types: ['normal'],
-      weight: 35,
-      height: 3,
-      attack: 56,
-      default: true,
-    },
-    {
-      name: 'salameche',
-      id: 5,
-      front_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-      back_pic_normal: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png',
-      shiny_pic_front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png',
-      shiny_pic_back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/4.png',
-      types: ['fire'],
-      weight: 85,
-      height: 6,
-      attack: 52,
-      default: true,
-    },
-  ];
+
+
+  apiResponse: boolean = false;
+
+  pokemonObject:any;
+  randomPokemonObject: any;
 
   default: boolean = true;
 
   constructor(private pokeApi: PokeapiService) { }
 
   ngOnInit(){
-    this.pokeApi.getRandomPokemon('dialga');
-  }
-  
-  toggleShiny(id: number){
-    for(let x = 0; x < this.cards.length; x++){
-      if(id == this.cards[x].id){
-        this.cards[x].default == true ? this.cards[x].default = false : this.cards[x].default = true;
-      } else {
-        console.log('continuer');
-      }
-    }
+    this.randomPokemonObject = this.pokeApi.getRandomPokemon();
   }
 
-  
+  showPokemon(pokemon: string){
+    this.pokeApi.getPokemon(pokemon)
+    .subscribe((data: any) => {
+      this.pokemonObject = data;
+      this.pokemonObject.default = true;
+    });
+
+    this.apiResponse = true; 
+  }
+
+  toggleResultPokemonShiny(){
+    this.pokemonObject.default == true ? this.pokemonObject.default = false : this.pokemonObject.default = true;
+  }
 }
--- src/app/pokeapi.service.ts
+++ src/app/pokeapi.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +7,25 @@ import { catchError, retry } from 'rxjs/operators';
 export class PokeapiService {
 
   url: string = 'https://pokeapi.co/api/v2/pokemon/';
+  randomNumbers: number[] = [];
+  randomPokemons: object[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getRandomPokemon(pokemon: string){
-    console.log(this.http.get(this.url + pokemon));
+  // Return an object about a wanted pokemon
+  getPokemon(pokemon: string){
+    return this.http.get(this.url + pokemon);
+  }
+
+  // Creates an array with 6 pokemon's informations as objects, which is used to make the 6 cards in the home page
+  getRandomPokemon(){
+      for(let i = 0; i < 6; i++){
+        let number = Math.floor(Math.random() * 905);
+        this.http.get(this.url + number).subscribe((data: any) => {
+        this.randomPokemons[i] = data;
+      });
+      }
+
+      return this.randomPokemons;
   }
 }
